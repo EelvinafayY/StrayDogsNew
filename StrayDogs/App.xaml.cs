eddb4b156d89d55993bd4c7a0a3bfe06b081c901
EelvinafayY <elvinafayzullina2005@icloud.com>
2025-01-29 13:52:30 +0300

    Добавьте файлы проекта.

---
+++ StrayDogs/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using MaterialDesignThemes.Wpf;
+using System.Windows.Media;
+using static MaterialDesignThemes.Wpf.Theme.ToolBar;
+
+namespace StrayDogs
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Настройка основной темы
+            var paletteHelper = new PaletteHelper();
+            var baseTheme = BaseTheme.Light;
+            var primaryColor = (Color)FindResource("PrimaryHueMid");
+            var secondaryColor = (Color)FindResource("SecondaryAccentMid");
+
+            // Создание и применение темы
+            var theme = Theme.Create(baseTheme, primaryColor, secondaryColor);
+            paletteHelper.SetTheme(theme);
+
+            // Установка других кистей (например, фона)
+            Application.Current.Resources["MaterialDesignPaper"] = FindResource("BackgroundPrimaryBrush");
+            Application.Current.Resources["MaterialDesignDivider"] = FindResource("BorderColorBrush");
+        }
+    }
+}
---
+++ StrayDogs/Pages/MainAdminPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StrayDogs.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainAdminPage.xaml
+    /// </summary>
+    public partial class MainAdminPage : Page
+    {
+        public MainAdminPage()
+        {
+            InitializeComponent();
+
+            List<string> list = new List<string>(){"Первый", "Second", "Third", "Fourht", "Five"};
+            VolierLv.ItemsSource = list;    
+
+            WorkersLv.ItemsSource = list;   
+
+        }
+
+        private void TabControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void probaI_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new ProbaPage());
+        }
+
+        private void Lv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ScrollUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (VolierLv.Items.Count > 0)
+            {
+                var index = VolierLv.SelectedIndex;
+                if (index > 0)
+                {
+                    VolierLv.SelectedIndex = index - 1;
+                    VolierLv.ScrollIntoView(VolierLv.SelectedItem);
+                }
+            }
+        }
+
+        private void ScrollDown_Click(object sender, RoutedEventArgs e)
+        {
+            if (VolierLv.Items.Count > 0)
+            {
+                var index = VolierLv.SelectedIndex;
+                if (index < VolierLv.Items.Count - 1)
+                {
+                    VolierLv.SelectedIndex = index + 1;
+                    VolierLv.ScrollIntoView(VolierLv.SelectedItem);
+                }
+            }
+        }
+    }
+}
